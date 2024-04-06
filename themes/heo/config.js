f2054f357492fa60b817bef77d8ec3efbd2e59c0
Gchen <abcdop6631@qq.com>
2024-04-06 18:10:06 +0800

    heo modify

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -17,7 +17,7 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_3: 'GCHEN.TECH',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
@@ -35,14 +35,14 @@ const CONFIG = {
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+  //  '🔍 分享与热心帮助',
+  //  '🤝 专修交互与设计',
+  //  '🏃 脚踏实地行动派',
+   // '🏠 智能家居小能手',
+    '🤖️ 数码科技爱好者'//,
+   // '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://github.com/Gchen77', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
